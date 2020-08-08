a24d0214d3ed8c2eeab03da82ba4064c1617fbf3
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-08 17:32:45 -0400

    birthday

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Sign from './Componenets/Sign'
 import HomePage from './Componenets/HomePage'
+import 'semantic-ui-css/semantic.min.css'
 
 export class App extends Component{
 
@@ -11,20 +12,29 @@ export class App extends Component{
     month: '',
     day: '',
     year: '',
-    sign: ''
+    sign: '',
+    celebs: []
   }
+
+  componentDidMount(){
+    fetch('https://zodiacal.herokuapp.com/api')
+    .then(res => res.json())
+    .then(celebs => console.log(celebs))
+  }
+
 
   handleOnChange = e => this.setState({ [e.target.name]: e.target.value })
 
   submitBirthday = () => console.log(this.state)
 
   render(){
+    console.log(this.state)
   return (
     <div className="App">
       <img src='https://res.cloudinary.com/teepublic/image/private/s--SIVNWQIj--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_c8e0ec,e_outline:48/co_c8e0ec,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1577739990/production/designs/7399322_0.jpg' alt='fortune teller' />
       <Switch>
         <Route path='/sign' render={()=> <Sign birthday={this.state.birthday}/>}/>
-        <Route path='/' render={()=> <HomePage submitBirthday={this.submitBirthday} handleOnChange={this.handleOnChange}/> } />
+        <Route path='/' render={()=> <HomePage day={this.state.day} month={this.state.month} year={this.state.year} submitBirthday={this.submitBirthday} handleOnChange={this.handleOnChange}/> } />
       </Switch>
     </div>
    )
--- src/Componenets/HomePage.js
+++ src/Componenets/HomePage.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
-const HomePage = () => {
-    return <div>Home</div>
+const HomePage = (props) => {
+    return (
+    <div>
+        <h1>Enter your date of birth</h1>
+        <select name='month' onChange={props.handleOnChange}>
+            <option value='jan'>Jan</option>
+            <option value='feb'>Feb</option>
+            <option value='march'>March</option>
+            <option value='april'>April</option>
+            <option value='may'>May</option>
+            <option value='june'>June</option>
+            <option value='july'>July</option>
+            <option value='aug'>Aug</option>
+            <option value='sept'>Sept</option>
+            <option value='oct'>Oct</option>
+            <option value='nov'>Nov</option>
+            <option value='dec'>Dec</option>
+        </select>
+        <select name='date' onChange={props.handleOnChange}>
+            {Array.from({length: 31}, (v, k) => k+1).map((date, index) => <option name='date' key={index} value={date}>{date}</option>)}
+        </select>
+           <input name='year' onChange={props.handleOnChange} name='year' value={props.year} />
+           <br></br>
+           <button className='ui button' onClick={props.submitBirthday}>Submit Birthday</button>
+    </div>
+    )
 }
 
 export default HomePage
