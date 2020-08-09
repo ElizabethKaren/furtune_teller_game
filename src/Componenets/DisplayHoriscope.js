fc2d30c372a60a301c535cd867c28f50e9d7853a
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-09 12:18:15 -0400

    signDetectedAndNewRoute

---
+++ src/Componenets/DisplayHoriscope.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DisplayHoriscope = props => {
+    if (!props.sign) return <div>No Sign Yet</div>
+    return (
+        <div></div>
+    )
+}
+
+export default DisplayHoriscope
