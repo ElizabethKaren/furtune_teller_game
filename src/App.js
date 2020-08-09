2e5747f59405c0369815503255971cc9fa4cc7ba
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-09 15:23:52 -0400

    iconChanged

--- src/App.js
+++ src/App.js
@@ -148,6 +148,8 @@ export class App extends Component{
     let mySign = this.state.signs.find(sign => sign.zodiac_sign === this.state.sign)
   return (
     <div className="App">
+      <br></br>
+      <br></br>
       <Nav sign={this.state.sign} birthday={this.state.birthday} />
       <img className='ui centered medium image' src='https://res.cloudinary.com/teepublic/image/private/s--SIVNWQIj--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_c8e0ec,e_outline:48/co_c8e0ec,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1577739990/production/designs/7399322_0.jpg' alt='fortune teller' />
       <Switch>
