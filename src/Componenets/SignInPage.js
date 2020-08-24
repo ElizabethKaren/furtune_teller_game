077eff8183223af76c3759cfc463bced712da794
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-24 14:19:03 -0400

    form

--- src/Componenets/SignInPage.js
+++ src/Componenets/SignInPage.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const SignInPage = props =>{
     return (
-        <div>
-            <input></input>
-            <input></input>
+        <div className='ui form'>
+            <div className='fields'>
+            <div className='field'>
+                <label>Email</label>
+                <input type='text' placeholder='user@furtuneteller.com'></input>
+            </div>
+            <div className='field'>
+                <label>Password</label>
+                 <input type='password'></input>
+            </div>
+            <button className='ui small button'>Submit</button>
+            </div>
         </div>
     )
 }
