1e941d94927976533a583c25c5416eb9577ddf72
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-24 16:05:32 -0400

    fixedWeirdIssue

--- src/Componenets/Nav.js
+++ src/Componenets/Nav.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Nav = props => {
     console.log(props.myMessages)
     if (!props.sign) return <div></div>
-    if (['A','E','I','O','U','Y'].includes(props.sign[0])) return <div><h3>An {props.sign} I see...</h3></div>
+    if (['A','E','I','O','U','Y'].includes(props.sign[0])) return <div><h3>An {props.sign} I see...</h3>{props.myMessages.length > 0 ? <Link to='/horiscope/inbox'><button className='ui small button'>{props.myMessages.length} Messages</button></Link> : null }</div>
     return (
         <div>
             <h3>A {props.sign} I see...</h3>
