51f0c5a5550acb02e9a29e4f3b7ff8357625ad83
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-30 11:13:35 -0400

    defaultAdded

--- src/App.js
+++ src/App.js
@@ -34,18 +34,43 @@ export class App extends Component{
     console.log(year)
     let animal = ''
     switch ((year - 4) % 12) {
-      case  0: animal = 'Rat';       break;
-      case  1: animal = 'Ox';            break;
-      case  2: animal = 'Tiger';     break;
-      case  3: animal = 'Rabbit';    break;
-      case  4: animal = 'Dragon';    break;
-      case  5: animal = 'Snake';     break;
-      case  6: animal = 'Horse';     break;
-      case  7: animal = 'Goat';  break;
-      case  8: animal = 'Monkey';    break;
-      case  9: animal = 'Rooster';   break;
-      case 10: animal = 'Dog';   break;
-      case 11: animal = 'Pig';   break;
+      case  0: animal = 'Rat';
+      break;
+      case  1:
+        animal = 'Ox';
+      break;
+      case  2:
+        animal = 'Tiger';
+      break;
+      case  3:
+        animal = 'Rabbit';
+      break;
+      case  4:
+        animal = 'Dragon';
+      break;
+      case  5:
+        animal = 'Snake';
+      break;
+      case  6:
+        animal = 'Horse';
+      break;
+      case  7:
+        animal = 'Goat';
+      break;
+      case  8:
+        animal = 'Monkey';
+      break;
+      case  9:
+        animal = 'Rooster';
+      break;
+      case 10:
+        animal = 'Dog';
+      break;
+      case 11:
+        animal = 'Pig';
+      break;
+      default:
+        animal = ''
   }
     this.setState({ animal: animal })
   }
