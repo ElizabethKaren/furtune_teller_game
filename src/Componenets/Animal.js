0ee5408824995de7d569310fa8193ee6c5dbc76b
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-30 14:06:07 -0400

    paddingTopChanged

--- src/Componenets/Animal.js
+++ src/Componenets/Animal.js
@@ -2,13 +2,12 @@ import React from 'react'
 import '../Styles/Animal.css'
 
 const Animal = props => {
-    console.log(props)
     if (!props.animal) return <div className='ui black label'>No Sign</div>
     return (
         <div id='chinesecenter'>
             <img className='ui medium image' src={props.picture} alt={props.animal} /> 
             <div className='ui black label'>
-                <h2>{props.animal}</h2>
+                <h2>Year of the {props.animal}</h2>
                 <h3>{props.description}</h3>
                 <h4>Best Matches: {props.bestMatch.map(match => match + ', ' )}</h4>
                 <h4>Better to Avoid: {props.avoid.map(match => match + ', ' )}</h4>
