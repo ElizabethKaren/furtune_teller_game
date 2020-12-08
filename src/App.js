f0e5bffa3369221058675cd2969bbcba2afcb4be
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-08 16:09:08 -0500

    timeChangePalm

--- src/App.js
+++ src/App.js
@@ -298,7 +298,7 @@ export class App extends Component{
     this.setState({ sign: sign })
   }
 
-  displayReading = () => setTimeout( () => this.setState({ displayReading: true }) , 7000)
+  displayReading = () => setTimeout( () => this.setState({ displayReading: true }) , 6000)
 
   clearReading = () => this.setState({ displayReading: false })
 
