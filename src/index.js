38d258b96fad7004c219a55ece8977d927ce05e1
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-09 11:30:47 -0500

    ugh

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router className='root'><App/></Router>, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <App />
+</Router>, 
+document.getElementById('root')
+);
 
 // ReactDOM.render(
 //   <React.StrictMode>
