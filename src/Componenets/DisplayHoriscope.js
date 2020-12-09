28027cceb0b14294d53650a8c24193b830f2c398
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-09 11:42:46 -0500

    overflowFIzed

--- src/Componenets/DisplayHoriscope.js
+++ src/Componenets/DisplayHoriscope.js
@@ -7,13 +7,13 @@ import Image from './Image'
 const DisplayHoriscope = props => {
     console.log(props.sign)
     if (props.sign === '' || props.sign === null ) return (
-        <div style={{ height: '100vh', backgroundColor: 'lightgrey' }}> 
+        <div style={{ overflow: 'scroll', height: '100vh', backgroundColor: 'lightgrey' }}> 
             <Link to='/birthday'><div id='out-locaiton' className='back'><strong>X</strong></div></Link>
             <h2><strong>No Sign Yet</strong></h2>
         </div>
     ) 
     return (
-        <div style={{ height: '100vh', backgroundColor: 'lightgrey' }}>
+        <div style={{ overflow: 'scroll', height: '100vh', backgroundColor: 'lightgrey' }}>
         <br></br>
         <br></br>
             <Nav sign={props.sign} birthday={props.birthday} />
--- src/Componenets/PalmReading.js
+++ src/Componenets/PalmReading.js
@@ -3,9 +3,9 @@ import '../Styles/PalmReading.css'
 import { Link } from 'react-router-dom'
 
 const PalmReading = props =>{
-    if (props.displayReading) return <div style={{ backgroundColor: 'black', height: '100vh' }}><div className='palm-message'> <Link to='/' onClick={props.clearReading}><div className='back'><strong>X</strong></div></Link><div className='ui black label'><h3>{props.fortune}</h3></div></div></div>
+    if (props.displayReading) return <div style={{ overflow: 'scroll', backgroundColor: 'black', height: '100vh' }}><div className='palm-message'> <Link to='/' onClick={props.clearReading}><div className='back'><strong>X</strong></div></Link><div className='ui black label'><h3>{props.fortune}</h3></div></div></div>
     return (
-        <div style={{ backgroundColor: 'black', height: '100vh' }}>
+        <div style={{ overflow: 'scroll', backgroundColor: 'black', height: '100vh' }}>
             <div className='palm-message'>
                 <div className='ui black label'>
                     <h3>Place your Palm on The Computer Screen</h3>
