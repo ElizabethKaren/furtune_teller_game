2a5cc43ba8d853966a6d089f52d1de863ecf8c19
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-09 15:52:39 -0500

    backgroundChange

--- src/Componenets/Animal.js
+++ src/Componenets/Animal.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Animal = props => {
     if (!props.animal) return <div className='noSign'><Link to='/birthday'><div id='location-back' className='back'><strong>X</strong></div></Link><div className='ui black label'> No Sign</div></div>
     return (
-        <div className='backGroundChange'>
+        <div>
             <div id='chinesecenter'>
                 <Link to='/birthday'><div id='location-back' className='back'><strong>X</strong></div></Link>
                 <img src={props.picture} alt={props.animal}  height="290" width="190"/> 
